4b08aef6bb43857bb5c104866a5351bebd48253e
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2019-10-27 20:17:54 -0400

    Add files via upload

---
+++ Aperture Tests/farfieldtesting.py
@@ -0,0 +1,7 @@
+# Here is were we attempt to create a far-field test for CAs
+
+import numpy
+import scipy
+
+''' Currently working on setting up code for transforming
+signals. This code will only have tests for far field. '''
