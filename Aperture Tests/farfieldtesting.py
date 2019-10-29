89ef438677d086cc9f709e667b3950a6a13eeedb
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2019-10-29 12:38:45 -0400

    Add files via upload

--- Aperture Tests/farfieldtesting.py
+++ Aperture Tests/farfieldtesting.py
@@ -1,6 +1,7 @@
 # Here is were we attempt to create a far-field test for CAs
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 ''' Currently working on setting up code for transforming
 signals. This code will only have tests for far field. '''
@@ -10,30 +11,23 @@ We will need to create a file (farfieldimage) that
 holds the array that represents the image for the far-field
 test'''
 
-np.load(farfieldimage.npy)
-N = size(farfieldimage.npy, 1)
+obj = np.load('farfieldimage.npy')
+N = np.size(obj, 1)
 
 # We begin with establishing the Fourier Transform functions
 
 F = lambda x: np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(x)))
 Ft = lambda x: np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(x)))
 
-# Next we pull the CA
+# Next we pull the CA, where the name if the file is the aperture received
 
-CA()
-
-# Creating the PSF
-
-PSF = np.zeros(N, N)
-
-# Creating the impulse response
-
-H_omega = F(PSF)
+CA = np.load('randomCM.npy')
 
 # Generating the image signal
 
-img_sig = Ft(F(farfieldimage.npy) * H)
+img_sig = Ft(F(obj) * F(PSF))
 
-''' This code is currently under major edits. It is likely
-that the FTs and setups will be established as a separate
-code that the tests call upon.'''
+img = F(img_sig)
+
+plt.imshow(abs(img))
+plt.show()
