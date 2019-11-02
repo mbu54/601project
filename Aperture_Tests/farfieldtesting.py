c6c9e9d73dae55187c540d30fe5067e3322baf68
Merlin Hoffman <merlin@Merlins-MacBook-Pro.local>
2019-11-02 10:58:38 -0400

    added tikhonov

--- Aperture_Tests/farfieldtesting.py
+++ Aperture_Tests/farfieldtesting.py
@@ -19,15 +19,38 @@ N = np.size(obj, 1)
 F = lambda x: np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(x)))
 Ft = lambda x: np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(x)))
 
+# Tikihonov reconstruction function
+Tk = lambda G,H,u: Ft((np.conj(H)*G)  / (abs(H)**2 + u))
+
 # Next we pull the CA, where the name if the file is the aperture received
 
 CA = np.load('randomCM.npy')
-
+'''
+psf = np.zeros((N,N))
+psf[49][49] = 1
+psf[49][50] = 1
+psf[50][49] = 1
+psf[50][50] = 1
+'''
 # Generating the image signal
+tfCA = F(CA)
+g = Ft(F(obj)*tfCA)
+G = F(g)
+#img = F(img_sig)
+u = np.logspace(-20,0,5)
 
-img_sig = Ft(F(obj) * F(PSF))
+#for i in range(len(u)):
+f_tk = Tk(G, tfCA, u[0])
 
-img = F(img_sig)
+#plt.figure(i)
+#plt.imshow(abs(f_tk)
 
-plt.imshow(abs(img))
+#plt.show()
+
+fig, ax = plt.subplots(1,3)
+
+ax[0].imshow(obj)
+ax[1].imshow(CA)
+ax[2].imshow(abs(f_tk))
+
 plt.show()
