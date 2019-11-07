9fcc67e44a5138d01afe22541b8025681a72289a
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2019-11-06 22:52:34 -0500

    Add files via upload

---
+++ Aperture_Tests/nearfieldimage.py
@@ -0,0 +1,2 @@
+''' Used to generate an image that represents
+an object in the near-field '''
---
+++ Aperture_Tests/nearfieldtesting.py
@@ -0,0 +1,7 @@
+''' In this code we implement an attempt and projecting
+an object in the near-field as a paraxial approximation
+along the propagation axis '''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
