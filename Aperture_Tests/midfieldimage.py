b30e6b55fa872c03aa40bd3d00ec15f19357ce23
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2019-11-20 16:51:33 -0500

    Add files via upload

---
+++ Aperture_Tests/midfieldimage.py
@@ -0,0 +1,31 @@
+'''
+This code makes a star input image to test 
+coded masks for mid-field
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+imageWidth = 100
+c = imageWidth//2-1
+
+image = np.zeros((imageWidth,imageWidth))
+for x in range(imageWidth):
+  for y in range(imageWidth):
+    if (x-c)**2+(y-c)**2 <= 50**2:
+      if (y-c)<-0.5*(x-c) and (y-c)>-2*(x-c):
+        image[x][y] = 1
+      elif (y-c)>-0.5*(x-c) and (y-c)<-2*(x-c):
+        image[x][y] = 1
+      elif (y-c)<2*(x-c) and (y-c)>0.5*(x-c):
+        image[x][y] = 1
+      elif (y-c)>2*(x-c) and (y-c)<0.5*(x-c):
+        image[x][y] = 1
+      elif y==c and x==c:
+        image[x][y] = 1
+
+np.save('midfieldimage.npy',image)
+
+# plot image
+plt.imshow(image)
+plt.show()
