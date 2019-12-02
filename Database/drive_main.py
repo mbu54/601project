ad686571f5533a52728c79c267a0fbbe8b027be3
HaikM17 <55334624+HaikM17@users.noreply.github.com>
2019-12-02 13:22:46 -0500

    Add files via upload

---
+++ Database/drive_main.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 19:30:46 2019
+
+@author: haikm
+"""
+
+from __future__ import print_function
+import httplib2
+import os, io
+import gspread
+import drive_auth
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from apiclient.http import MediaFileUpload, MediaIoBaseDownload
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+
+#Drive API setup
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = 'credentials.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+authInst = drive_auth.auth(SCOPES,CLIENT_SECRET_FILE,APPLICATION_NAME)
+credentials = authInst.getCredentials()
+
+http = credentials.authorize(httplib2.Http())
+drive_service = discovery.build('drive', 'v3', http=http)
+
+#Sheets API setup
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds= ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+client=gspread.authorize(creds)
+sheet=client.open("Coded Aperture Database").sheet1
+
+def listFiles(size):
+    results = drive_service.files().list(
+        pageSize=size,fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print('{0} ({1})'.format(item['name'], item['id']))
+
+def downloadFile(file_id,filepath):
+    request = drive_service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+    with io.open(filepath,'wb') as f:
+        fh.seek(0)
+        f.write(fh.read())
+
+def createFolder(name):
+    file_metadata = {
+    'name': name,
+    'mimeType': 'application/vnd.google-apps.folder'
+    }
+    file = drive_service.files().create(body=file_metadata,
+                                        fields='id').execute()
+    print ('Folder ID: %s' % file.get('id'))
+    
+def uploadFile(filename,filepath,mimetype):
+    file_metadata = {'name': filename}
+    media = MediaFileUpload(filepath,
+                            mimetype=mimetype)
+    file = drive_service.files().create(body=file_metadata,
+                                        media_body=media,
+                                        fields='id').execute()
+    print('File ID: %s' % file.get('id'))
+    
+def uploadFileFolder(name,filepath,mimetype):
+    folder_id = '1HwU840PsR4pqAq4fCjEl6UeXJO_DDvk3'
+    file_metadata = {
+        'name': name,
+        'parents': [folder_id]
+    }
+    media = MediaFileUpload(filepath,
+                            mimetype=mimetype,
+                            resumable=True)
+    file = drive_service.files().create(body=file_metadata,
+                                        media_body=media,
+                                        fields='id').execute()
+    print ('File ID: %s' % file.get('id'))
+
+def searchFile(size,query):
+    results = drive_service.files().list(
+    pageSize=size,fields="nextPageToken, files(id, name, kind, mimeType)",q=query).execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(item)
+            print('{0} ({1})'.format(item['name'], item['id']))
+#uploadFile('unnamed.jpg','unnamed.jpg','image/jpeg')
+#downloadFile('1Knxs5kRAMnoH5fivGeNsdrj_SIgLiqzV','google.jpg')
+#createFolder('Google')
+uploadFileFolder('test1.csv','test1.csv','text/csv')
+insertRow=['Aperture Type', 'Name', 'Association']
+sheet.insert_row(insertRow,2)
