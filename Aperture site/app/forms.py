37d18800bbcb0308cb8a9ee1dd8b967f6149821b
edecarli <55355755+edecarli@users.noreply.github.com>
2019-12-03 13:23:53 -0500

    Update forms.py

--- Aperture site/app/forms.py
+++ Aperture site/app/forms.py
@@ -6,5 +6,5 @@ class LoginForm(FlaskForm):
   CA = StringField('Coded aperture name', validators=[DataRequired()])
   author = StringField('Author name', validators=[DataRequired()])
   assoc = StringField('Association', validators=[DataRequired()])
-  #mask = SubmitField('Upload mask')
+  mask = SubmitField('Upload mask')
   submit = SubmitField('Submit')
